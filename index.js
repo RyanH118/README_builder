31980951163c5d6658a54e6aa6d1e3f2c5b461c5
ryanh <ryanhinson118@mail.com>
2024-04-24 18:00:12 -0400

    Created function to write the README file.

--- index.js
+++ index.js
@@ -24,42 +24,46 @@ const questions = [
         type: 'input',
         name: 'installation',
         message: 'What is the installation process?'
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'usage',
         message: 'How do you use the project?'
-      },
-      {
+    },
+    {
         type: 'list',
         name: 'license',
         message: 'Choose a license for your project:',
         choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'Unlicense']
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'contributing',
         message: 'How can you contribute?'
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'tests',
         message: 'What tests can you run?'
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'githubUsername',
         message: 'What is your github username?'
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'email',
         message: 'What is your email?'
-      },
-    ];
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log('success!');
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() { }
